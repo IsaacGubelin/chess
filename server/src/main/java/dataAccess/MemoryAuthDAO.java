6f27fcdd8d89ddb4b78365c0e37e5f5581aa567b
Isaac Gubelin <isaacgubelin@gmail.com>
2024-02-21 12:18:41 -0700

    Updated authDAO

--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,4 +1,27 @@
 package dataAccess;
 
 public class MemoryAuthDAO implements AuthDAO {
+
+    // TODO:
+
+    // Create
+    // Read
+    // Update
+    // Delete
+
+    // Add these methods:
+    // clear
+    // createUser
+    // getUser
+    // createGame
+    // getGame
+    // listGames
+    // updateGame
+    // createAuth
+    // getAuth
+    // deleteAuth
+
+    // Use DataAccessException if user tries to plug in something that already exists
+    // Or removes something that doesn't exist
+
 }
