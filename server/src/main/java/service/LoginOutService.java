a6a2398c9eceb1e5b47dc12fa55c2632120a46b0
Isaac Gubelin <isaacgubelin@gmail.com>
2024-02-23 12:56:37 -0700

    Work started on login logout handler

--- server/src/main/java/service/LoginOutService.java
+++ server/src/main/java/service/LoginOutService.java
@@ -1,12 +1,14 @@
 package service;
 
-public class LoginOut {
+public class LoginOutService {
+
+    //TODO:
+    // Handler gets the json. Converts into object. Sends to this service.
 
     public model.AuthData login(model.UserData user) {
 
         //fixme
         return null;
-
     }
     public void logout(model.UserData user) {}
 
